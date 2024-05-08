415091f0a51abf680cb2ecd7841a0d975404c9ff
FullTimeScam <418.gingin@gmail.com>
2024-05-08 14:13:13 +0900

    컴포넌트 전부 분리 (폴더링 하면 더 좋을 듯)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Footer from "./components/Footer";
-import Header from "./components/Header_";
+import Header from "./components/Header";
 import Section from "./components/Section";
 const App = () => {
   return (
