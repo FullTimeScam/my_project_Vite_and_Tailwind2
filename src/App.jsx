fd81b5f885dde65f22d38e9693bb559becd429c2
FullTimeScam <418.gingin@gmail.com>
2024-05-08 14:11:45 +0900

    컴포넌트 전부 분리 (폴더링 하면 더 좋을 듯)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
-// const App = () => {
-//   return (
-//     <div className="bg-blue-300 rounded-md hover:bg-red-400 md:bg-red-100 text-4xl">
-//       Hello React and Tailwind2!
-//     </div>
-//   );
-// };
-
 import Footer from "./components/Footer";
 import Header from "./components/Header_";
 import Section from "./components/Section";
-
-// export default App;
-
-// VITE가이드 보고 시작하면 됨
-//https://vitejs.dev/guide/
-
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto flex flex-col">
@@ -24,6 +10,7 @@ const App = () => {
       {/*섹션*/}
       <Section />
 
+      {/*푸터*/}
       <Footer />
     </div>
   );
