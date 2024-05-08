c0828d3426f21260d86ec110dcf775f5e9f52327
FullTimeScam <418.gingin@gmail.com>
2024-05-08 14:19:56 +0900

    컴포넌트 - 폴더링 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Section from "./components/Section";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Section from "./components/Section/Section";
 const App = () => {
   return (
     <div className="bg-red-100 min-h-screen max-w-screen-md mx-auto flex flex-col">
